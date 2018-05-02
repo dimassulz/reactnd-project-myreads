cb255cc2c33f2fc6e8155612a2181a5deba37b33
Dimas Sulz <dimassulz@gmail.com>
2018-05-02 02:14:30 -0300

    Resolvido bug author

--- src/Book.js
+++ src/Book.js
@@ -12,7 +12,7 @@ class Book extends Component {
         return(
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail})` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.smallThumbnail ? book.imageLinks.smallThumbnail: 'https://www.marjon.ac.uk/margen/img/blankBook.png'})` }}></div>
                 <div className="book-shelf-changer">
                     <select value={book.shelf} onChange={(e) => onUpdateShelf(book, e.target.value)}>
                         <option value="none" disabled>Move to...</option>
@@ -24,7 +24,10 @@ class Book extends Component {
                 </div>
             </div>
             <div className="book-title"><a href={book.previewLink} target="_blank">{book.title}</a></div>
-            {book.authors.map((author,key) => ( <div className="book-authors" key={key}> {author} </div> ))}
+             {book.authors !== undefined ? 
+                book.authors.map((author,key) => ( <div className="book-authors" key={key}> {author} </div> )) : (
+                    <div className="book-authors"> Without Author </div>
+            )}
         </div>
       )
     }
